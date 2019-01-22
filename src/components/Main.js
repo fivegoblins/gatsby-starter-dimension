fa80047c32df45338b10a6424af1c45973e4948c
Alexandra <alexswartz9753@gmail.com>
2019-01-22 12:34:44 -0500

    image changes and added ig project to work section

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,6 +4,8 @@ import PropTypes from 'prop-types'
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
+import bg from '../images/bg.jpg'
+import bg3 from '../images/bg3.jpg'
 
 class Main extends React.Component {
   render() {
@@ -15,7 +17,7 @@ class Main extends React.Component {
 
         <article id="intro" className={`${this.props.article === 'intro' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Intro</h2>
-          <span className="image main"><img src={pic01} alt="" /></span>
+          <span className="image main"><img src={bg3} alt="" /></span>
           <p>Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet egestas mauris. Ut magna finibus nisi nec lacinia. Nam maximus erat id euismod egestas. By the way, check out my <a href="#work">awesome work</a>.</p>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor fermentum. Nullam venenatis erat id vehicula viverra. Nunc ultrices eros ut ultricies condimentum. Mauris risus lacus, blandit sit amet venenatis non, bibendum vitae dolor. Nunc lorem mauris, fringilla in aliquam at, euismod in lectus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. In non lorem sit amet elit placerat maximus. Pellentesque aliquam maximus risus, vel sed vehicula.</p>
           {close}
@@ -23,9 +25,9 @@ class Main extends React.Component {
 
         <article id="work" className={`${this.props.article === 'work' ? 'active' : ''} ${this.props.articleTimeout ? 'timeout' : ''}`} style={{display:'none'}}>
           <h2 className="major">Work</h2>
-          <span className="image main"><img src={pic02} alt="" /></span>
-          <p>Adipiscing magna sed dolor elit. Praesent eleifend dignissim arcu, at eleifend sapien imperdiet ac. Aliquam erat volutpat. Praesent urna nisi, fringila lorem et vehicula lacinia quam. Integer sollicitudin mauris nec lorem luctus ultrices.</p>
-          <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus pharetra. Pellentesque condimentum sem. In efficitur ligula tate urna. Maecenas laoreet massa vel lacinia pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis libero. Mauris aliquet magna magna sed nunc rhoncus amet feugiat tempus.</p>
+          <span className="image main"><img src={bg} alt="" /></span>
+          <a href='https://frosty-northcutt-395da9.netlify.com/' className='work-link'>Instagram Clone</a>
+            <p className='work-description'>Front-end application based on Instagram's login page and feed. Built with React.</p>
           {close}
         </article>
 
@@ -40,7 +42,6 @@ class Main extends React.Component {
           <h2 className="major">Contact</h2>
           <ul className="icons">
             <li><a href="https://twitter.com/twelvegoblins" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-            <li><a href="https://instagram.com/fivegoblins" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
             <li><a href="https://github.com/fivegoblins" className="icon fa-github"><span className="label">GitHub</span></a></li>
             <li><a href="https://www.linkedin.com/in/alexandra-swartz-320a27173/" className="icon fa-linkedin"><span className="label">LinkedIn</span></a></li>
 
